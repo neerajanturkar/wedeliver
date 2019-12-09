93dedda96cc09701b72652c479415ca6c6c0902f
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2019-12-09 18:48:06 +0100

    merge commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatInputModule , MatIconModule, MatProgressSpinnerModule , MatButtonToggleModule, MatDividerModule, MatCardModule , MatSlideToggleModule, MatCheckboxModule ,MatFormFieldModule, MatSliderModule} from '@angular/material';
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {
+  MatToolbarModule,
+  MatInputModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatButtonToggleModule,
+  MatDividerModule,
+  MatCardModule,
+  MatSlideToggleModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatSliderModule
+} from "@angular/material";
+import { AppComponent } from "./app.component";
 
-import { HeaderComponent } from './header/header.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule , routingComponents} from './app-routing.module';
-import { FormsModule,  } from '@angular/forms'
-import { MapService } from './map.service';
+import { HeaderComponent } from "./header/header.component";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule, routingComponents } from "./app-routing.module";
+import { FormsModule } from "@angular/forms";
+import { MapService } from "./map.service";
 
-import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
+import {
+  DlDateTimeDateModule,
+  DlDateTimePickerModule
+} from "angular-bootstrap-datetimepicker";
 // import {MatCheckboxModule} from '@angular/material/checkbox';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { SuccessComponent } from './success/success.component';
-import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SuccessComponent } from "./success/success.component";
+import { LoginComponent } from "./login/login.component";
 
-=======
 import { LoadingService } from "./loading.service";
->>>>>>> eb557f9b565302500c4d6d9290264eff5cb01fd9
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
     HeaderComponent,
-<<<<<<< HEAD
-    OrderComponent,
+
     SuccessComponent,
     LoginComponent
-=======
->>>>>>> eb557f9b565302500c4d6d9290264eff5cb01fd9
   ],
   imports: [
     BrowserModule,
@@ -51,4 +60,4 @@ import { LoadingService } from "./loading.service";
   providers: [MapService, LoadingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-success',
-  templateUrl: './success.component.html',
-  styleUrls: ['./success.component.css']
+  selector: "app-success",
+  templateUrl: "./success.component.html",
+  styleUrls: ["./success.component.css"]
 })
 export class SuccessComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
